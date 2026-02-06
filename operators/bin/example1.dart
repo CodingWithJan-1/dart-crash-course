0e2a985915e4b48a748843033eb4ed79ef611c9c
Jan 3rd bitoon <jbitoon03@gmail.com>
2026-02-06 16:53:07 +0800

    feat: add operators chapter

---
+++ operators/bin/example1.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  const age1 = 60.1;
+  const age2 = 30.2;
+  print(age1 + age2); 
+  print(age1 + age2 + 30); 
+  print(age1 - age2); 
+  print(age1 * age2);
+  const double devided = age1 / age2;
+  print(devided);
+  const int intDevided = age1 ~/ age2;
+  print(intDevided);
+}
---
+++ operators/bin/example3.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // unary postfix
+  int age = 30;
+
+  print(age--);
+  print(age);
+  print(age++);
+  print(age);
+}
